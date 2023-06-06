e54362d5c74e5db38c16c3b0e57d92a16c666ad9
gabrielhochmann <gabrielhochmannt@gmail.com>
2023-06-05 23:48:51 -0400

    Upgrade exercise 1 from chapter 4 - Conditions, Relational Operators, Css.

---
+++ cap04/js/ex4_1.js
@@ -0,0 +1,25 @@
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#outAverage");
+const resp2 = document.querySelector("#outSituation");
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const name = frm.inName.value
+    const firstGrade = Number(frm.inFirstGrade.value)
+    const secondGrade = Number(frm.inSecondGrade.value)
+
+    const average = (firstGrade + secondGrade) / 2;
+
+    resp1.innerText = `Average Grade of ${average.toFixed(2)}`
+
+    if (average >= 7.0) {
+        resp2.innerText = `Congratulations ${name}! You have been approved!`
+        resp2.style.color = "blue";
+    } else {
+        resp2.innerText = `Oops ${name}! You have been failed :(`
+        resp2.style.color = "red";
+    };
+
+    resp2.innerText = `${situation}`
+})
